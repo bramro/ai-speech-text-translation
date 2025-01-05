47851f98ce3a2d0ed2fe4f3ba6ed5872aca1b4d4
Bram Rooijmans <b.rooijmans@academylabs.nl>
2025-01-05 16:03:20 +0100

    imports

--- src/TextTranslator.js
+++ src/TextTranslator.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { env, pipeline } from '@xenova/transformers';
 import Spinner from './Spinner';
 
