ac6e1663e873714e25e84c537b2e7823f544f85c
Bram Rooijmans <b.rooijmans@academylabs.nl>
2025-01-05 15:50:37 +0100

    Google Analytics only in final build

--- src/App.js
+++ src/App.js
@@ -1,31 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-import Navbar from './Navbar'; 
+import Navbar from './Navbar';
 import Intro from './Intro';
 import Left from './Left';
-import Right from './Right'; 
+import Right from './Right';
 
 function App() {
+
+  useEffect(() => {
+    if (process.env.REACT_APP_ENV === "production") {
+      // Dynamically load the Google Analytics script
+      const script = document.createElement("script");
+      script.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.REACT_APP_GA_ID}`;
+      script.async = true;
+      document.head.appendChild(script);
+
+      // Initialize Google Analytics
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        window.dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+      gtag("config", process.env.REACT_APP_GA_ID);
+    }
+  }, []);
 
   const [text, setText] = useState('');
 
   return (
     <div className="antialiased bg-gray-100 min-h-screen">
 
-      <Navbar/> 
+      <Navbar />
 
       <main className="container mx-auto mt-8 p-4">
 
-        <Intro/>
+        <Intro />
 
         <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
           <Left text={text} setText={setText} />
-          <Right text={text}/>
+          <Right text={text} />
         </div>
       </main>
 
       <footer className="bg-gray-800 p-4 text-center text-white mt-8">
-        <p>&#169; {new Date().getFullYear()} Academy Labs</p>
+        <p>&#169; {new Date().getFullYear()} <a href="https://www.academylabs.nl">Academy Labs</a></p>
       </footer>
     </div>
   );
